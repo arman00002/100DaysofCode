525fc452ab44c8a6efbe4659b4d304d840d11374
arman00002 <armanpattnaikbrb@gmail.com>
2025-04-05 02:26:59 +0530

    Day 4 Updated

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,4 @@
+import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+turn=random.randint(0,4)
+print(f"{friends[turn]} has to pay.")
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -24,3 +24,35 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+import random
+choices=[rock,paper,scissors]
+name=["Rock","Paper","Scissors"]
+player=int(input("What do you choose? 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+computer=random.randint(0,2)
+print("You chose:\n")
+print(name[player],"\n")
+print(choices[player])
+print("Computer chose:\n")
+print(name[computer],"\n")
+print(choices[computer])
+if player==0:
+    if computer==0:
+        print("\nDraw")
+    elif computer==1:
+        print("\nYou Lose")
+    elif computer==2:
+        print("\nYou Win")
+elif player==1:
+    if computer==0:
+        print("\nYou Win")
+    elif computer==1:
+        print("\nDraw")
+    elif computer==2:
+        print("\nYou Lose")
+elif player==2:
+    if computer==0:
+        print("\nYou Lose")
+    elif computer==1:
+        print("\nYou Win")
+    elif computer==2:
+        print("\nDraw")
