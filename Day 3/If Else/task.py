61cfbd4070e9c9389dae8e9c8a82044526b20da4
arman00002 <armanpattnaikbrb@gmail.com>
2025-04-05 00:30:18 +0530

    Day 3 Updated

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+if height>=170:
+    print("Allowed")
+else:
+    print("Not Allowed")
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -2,4 +2,23 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
-
+bill=0
+if size=='s' or size=='S':
+    bill+=15
+    if pepperoni=='y' or pepperoni=='Y':
+        bill+=2
+    if extra_cheese=='y' or extra_cheese=='Y':
+        bill+=1
+elif size=='m' or size=='M':
+    bill+=20
+    if pepperoni=='y' or pepperoni=='Y':
+        bill+=3
+    if extra_cheese=='y' or extra_cheese=='Y':
+        bill+=1
+elif size=='l' or size=='L':
+    bill+=25
+    if pepperoni=='y' or pepperoni=='Y':
+        bill+=3
+    if extra_cheese=='y' or extra_cheese=='Y':
+        bill+=1
+print(f"Your final bill is: ${bill}.")
