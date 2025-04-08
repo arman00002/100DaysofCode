8b010f64a02989d6e291056641cef73f01cdb91d
arman00002 <armanpattnaikbrb@gmail.com>
2025-04-08 06:31:26 +0530

    Day 8 Updated

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -1,9 +1,8 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
 
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
@@ -12,15 +11,24 @@ shift = int(input("Type the shift number:\n"))
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
 def encrypt(original_text, shift_amount):
-    cipher_text = ""
+    encrypted = ""
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        base=ord('a')
+        shifted=(ord(letter)-base+shift_amount)%26+base
+        encrypted+=chr(shifted)
+    print(f"Here is the encoded result: {encrypted}")
+def decrypt(original_text,shift_amount):
+    decrypted = ""
+    for letter in original_text:
+        base = ord('a')
+        shifted = (ord(letter) - base - shift_amount) % 26 + base
+        decrypted += chr(shifted)
+    print(f"Here is the decoded result: {decrypted}")
 
+def caesar_cipher(original_text,shift_value,choice):
+    if choice=='encode':
+        encrypt(original_text,shift_value)
+    elif choice=='decode':
+        decrypt(original_text,shift_value)
 
-encrypt(original_text=text, shift_amount=shift)
-
-
-
+caesar_cipher(text,shift,direction)
