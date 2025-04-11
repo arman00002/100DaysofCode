96edfcefbe683c07b898266358e540278fa002b9
arman00002 <armanpattnaikbrb@gmail.com>
2025-04-12 00:02:10 +0530

    Day 10

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,30 @@
+import art
 def add(n1, n2):
     return n1 + n2
+def sub(n1,n2):
+    return n1-n2
+def mul(n1,n2):
+    return n1*n2
+def div(n1,n2):
+    return n1/n2
+dict={"+":add,"-":sub,"*":mul,"/":div}
 
+def calculator():
+    print(art.logo)
+    cont=True
+    n1 = float(input("Enter First number"))
+    while cont:
+        for i in dict:
+            print(i)
+        op=input("Enter operator")
+        n2=float(input("Enter Second number"))
+        result=(dict[op](n1,n2))
+        choice=(f'Type "y" to continue calculation with {result}, type "n" to start new calculation')
+        if choice=="y":
+            num1=result
+        else:
+            cont=False
+            print("\n"*20)
+            calculator()
 
+calculator()
--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -5,8 +5,9 @@ def format_name(f_name, l_name):
 
 
 formatted_name = format_name("AnGeLa", "YU")
-
+print(formatted_name)
+"""Length of formatted length"""
 length = len(formatted_name)
-
+print(length)
 
 
